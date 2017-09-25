db138000a0b858479378fbfcab0851c2bf122794
David Stonikas <dstonikas@gmail.com>
2017-09-25 20:34:21 +0000

    Got all non-optional snacks to appear on index page

--- app/client/snack_api.rb
+++ app/client/snack_api.rb
@@ -1,43 +1,38 @@
-# class SnackAPI
-#   include HTTParty
-#   base_uri 'https://api-snacks.nerderylabs.com/v1/'
-#   format :json
-#
-#
-#   def initialize(*opts)
-#     @options = opts.reverse_merge({
-#       ApiKey: ENV['SNACK_API_KEY']
-#       })
-#   end
-#
-#   def get_snacks
-#     response = self.class.get('/snacks', @options)
-#   end
-# end
 class SnackAPI
   include HTTParty
-  base uri 'https://api-snacks.nerderylabs.com/v1/'
+  base_uri 'https://api-snacks.nerderylabs.com/v1/'
   SNACK_ACCESS = "/snacks?ApiKey=#{ENV['SNACK_API_KEY']}"
+
+  def initalize
+    @all_snacks = []
+  end
 
   def get_snacks
     response = self.class.get(SNACK_ACCESS)
-    JSON.parse(response.body)
+
+    @all_snacks = JSON.parse(response.body)
   end
 
-  def post_snacks(snacks)
-  snacks.each do |snack|
-    options = {
-      body: {
-        name: snack.name,
-        location: snack.purchase_location,
-        lastPurchaseDate: snack.last_purchased
-      }.to_json,
-      headers: {
-        'Content-Type' => 'application/json'
-      }
-    }
-
-    response = self.class.post(SNACK_ACCESS, options)
-
+  def where(optional: true)
+    @all_snacks.select do |snack|
+      snack["optional"] == optional
+    end
   end
+
+  # def post_snacks(snacks)
+  # snacks.each do |snack|
+  #   options = {
+  #     body: {
+  #       name: snack.name,
+  #       location: snack.purchase_location,
+  #       lastPurchaseDate: snack.last_purchased
+  #     }.to_json,
+  #     headers: {
+  #       'Content-Type' => 'application/json'
+  #     }
+  #   }
+  #   end
+  #   response = self.class.post(SNACK_ACCESS, options)
+  #
+  # end
 end
--- app/controllers/snacks_controller.rb
+++ app/controllers/snacks_controller.rb
@@ -1,18 +1,20 @@
 class SnacksController < ApplicationController
   def index
-    @permanent_snacks = Snack.where(optional: false)
-    @optional_snacks = Snack.where(optional: true) #going to be added a suggested field to the optional snacks.
+    snack_api = SnackAPI.new
+    snack_api.get_snacks
+    @permanent_snacks = snack_api.where(optional: false)
+    @optional_snacks = snack_api.where(optional: true) #going to be added a suggested field to the optional snacks.
   end
 
-  # def new
-  # end
-  #
-  # def create
-  # end
-  #
-  # def update
-  # end
-  #
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
   # private
   #
   # def snack_params
